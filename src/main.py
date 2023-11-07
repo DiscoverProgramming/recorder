d0beca0b690b6930460691256f510badc902f763
DiscoverProgramming <150089635+DiscoverProgramming@users.noreply.github.com>
2023-11-07 10:24:19 -0500

    Update and rename main.py to src/main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ start_index = int(input("Starting index for image names: "))
 interval = float(input("Interval between frames shown on video(Seconds): "))
 
 
-capture = lib(delay=delay, start_index=start_index, dir="imgs/", interval=interval)
+capture = lib(delay=delay, start_index=start_index, dir="../imgs/", interval=interval)
 
 capture.run()
